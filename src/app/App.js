27291b7acbe8ef2023eff5d27be7aa29de6b7e0c
anducnguyen <lordmasterking001@gmail.com>
2022-04-29 00:51:21 +0700

    d

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "@material-ui/core";
 
-import Header from "../Components/Header/Header";
+import Header from "../components/Header/Header";
 import setAuthToken from "../redux/auth/auth.setToken";
 import Router from "./routes/routes";
 
-import Alertbar from "../Components/Alert/Alert";
+import Alertbar from "../components/Alert/Alert";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
